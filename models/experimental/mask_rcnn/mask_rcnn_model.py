50f555ca4eea21bf2176a6c0f2b51ce7ab7249a6
BRougeHub2 <48873876+BRougeHub2@users.noreply.github.com>
2019-03-24 00:38:57 -0500

    Update mask_rcnn_model.py

--- models/experimental/mask_rcnn/mask_rcnn_model.py
+++ models/experimental/mask_rcnn/mask_rcnn_model.py
@@ -424,7 +424,7 @@ def _model_fn(features, labels, mode, params, variable_filter_fn=None):
       def pre_trained_scaffold_fn():
         """Loads pretrained model through scaffold function."""
         # Exclude all variable of optimizer.
-        prefix = 'mask_head/'
+        prefix = 'd/'
         vars_to_load = _build_assigment_map(
             optimizer,
             prefix=prefix,
